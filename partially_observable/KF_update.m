e5f59087147ed33a4f92be7f49b8e40e506cb576
Polaris <1223089722@qq.com>
2022-05-20 15:07:43 +0800

    Correct wrong formula

--- partially_observable/KF_update.m
+++ partially_observable/KF_update.m
@@ -6,9 +6,9 @@ function [x_kf_u,P_kf_u] = KF_update(x_kf_p, P_kf_p, y, R)
         0, 0, 0, 0;
     ];
     innovation = y - C * x_kf_p; 
-    lamda_t = P_kf_p + R;
-    kalman_gain = P_kf_p * inv(lamda_t);
+    lamda_t = C * P_kf_p * C' + R;
+    kalman_gain = P_kf_p * C' * inv(lamda_t);
     
     x_kf_u = x_kf_p + kalman_gain * innovation;
-    P_kf_u = P_kf_p - kalman_gain * P_kf_p;
+    P_kf_u = P_kf_p - kalman_gain * C * P_kf_p;
 end
